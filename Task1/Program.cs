98fe60b4d11a97d34764ff741bdc7667f9952290
skripkalisa <skripkalisa.msk@ya.ru>
2021-08-21 15:47:46 +0300

    Tasks 1 and 2 added

---
+++ Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Task1
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                IterateExceptions();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Не получилось вывести список исключений: " + e.Message);
+            }
+        }
+
+        private static void IterateExceptions()
+        {
+            foreach (var exception in new ExceptionsArray().Exceptions)
+            {
+                try
+                {
+                    exception.Data.Add("Дата создания исключения : ", DateTime.Now);
+                    throw exception;
+                }
+                catch (Exception e)
+                {
+                    foreach (DictionaryEntry en in e.Data)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.WriteLine(en.Key + en.Value?.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
